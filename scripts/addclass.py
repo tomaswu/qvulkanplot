efa1f1ff37e4898e26a67eea32145e23df1ccbf8
Tomas <tomasiwt@gmail.com>
2024-04-23 18:12:36 +0800

    backup

---
+++ scripts/addclass.py
@@ -0,0 +1,14 @@
+#! python3
+# -*- encoding: utf-8 -*-
+"""
+@File    :   addclass.py
+@Time    :   24-04-23
+@Version :   1.0
+@Author  :   Tomas 
+@Contact :   tomaswu@qq.com
+@Desc    :   
+"""
+
+import argparse
+
+parser = argparse.ArgumentParser()
